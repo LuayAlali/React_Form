e7440488e5b5e309c6eede238864e726e706cc4b
Luay Alali <luayalali@gmail.com>
2020-12-14 14:23:47 -0800

    installed react and materialui

---
+++ practice/src/components/UserForm.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export class UserForm extends Component {
+    state={
+        step: 1,
+        firstName: '',
+        lastName: '',
+        Email: '',
+        Occupation: '',
+        City: '',
+        Bio: '',
+
+    }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default UserForm
